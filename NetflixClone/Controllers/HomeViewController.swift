64592a03cee9c552668b84c246958601589da6ff
John Erick Santos <johnsantosdev@gmail.com>
2023-05-29 04:45:50 +1000

    feat: Dequeue re usable tableview cell

--- NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/Controllers/HomeViewController.swift
@@ -13,17 +13,17 @@ class HomeViewController: UIViewController {
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
         return table
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         view.addSubview(homeFeedTable)
-
+        
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
@@ -34,18 +34,23 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello, world"
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: CollectionViewTableViewCell.identifier,
+            for: indexPath) as? CollectionViewTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        
         return cell
     }
-
+    
     // TODO: Fix heights
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
-
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
     }
