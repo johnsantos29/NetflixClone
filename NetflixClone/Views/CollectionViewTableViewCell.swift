ebed4bde4d5738f764cf95542340071e5167ffec
John Erick Santos <johnsantosdev@gmail.com>
2023-05-29 05:06:02 +1000

    feat: Define height in layout - UICollectionViewFlowLayout horizontal achieves a 'carousel'

--- NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -9,28 +9,29 @@ import UIKit
 
 class CollectionViewTableViewCell: UITableViewCell {
     static let identifier = "CollectionViewTableViewCell"
-
+    
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
         layout.scrollDirection = .horizontal
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-
+        
         return collectionView
     }()
-
+    
     // MARK: - Init
-
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemPink
-
+        
         contentView.addSubview(collectionView)
-
+        
         collectionView.delegate = self
         collectionView.dataSource = self
     }
-
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("Unsupported")
@@ -48,7 +49,7 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
         cell.backgroundColor = .systemGreen
         return cell
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
