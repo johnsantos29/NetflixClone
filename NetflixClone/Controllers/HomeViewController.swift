94791d9efcd22d7d7d0b374247f28a9454347f28
John Erick Santos <johnsantosdev@gmail.com>
2023-05-28 12:02:47 +1000

    feat: HomeViewController - define homeFeedTable UITableView

--- NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/Controllers/HomeViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forHeaderFooterViewReuseIdentifier: "cell")
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .systemBackground
     }
 }
