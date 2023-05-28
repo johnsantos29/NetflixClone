b305e36bdc1ebe57e65b08655d5a9c91ce1f055f
John Erick Santos <johnsantosdev@gmail.com>
2023-05-28 11:36:32 +1000

    feat: Create Cocoa Touch class - HomeViewController

---
+++ NetflixClone/Controllers/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  NetflixClone
+//
+//  Created by John Erick Santos on 28/5/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
