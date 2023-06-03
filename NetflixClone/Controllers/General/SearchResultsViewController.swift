b51263ded2b4f5b2c97a157d3a622c870fdbf970
John Erick Santos <johnsantosdev@gmail.com>
2023-06-04 06:48:25 +1000

    chore: reorganise view controllers, init search results view controller

---
+++ NetflixClone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResultsViewController.swift
+//  NetflixClone
+//
+//  Created by John Erick Santos on 4/6/2023.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+}
