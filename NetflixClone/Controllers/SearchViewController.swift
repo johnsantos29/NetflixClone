9968ab7dd88e979cfecbec222b2cdaddd648a37b
John Erick Santos <johnsantosdev@gmail.com>
2023-06-04 06:25:05 +1000

    feat: Initialise SearchViewController title and Navigation

--- NetflixClone/Controllers/SearchViewController.swift
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
     }
 }
