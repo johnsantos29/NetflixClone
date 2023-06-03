8b5be52d64a03f466a0e101790d1bd4fa9c105df
John Erick Santos <johnsantosdev@gmail.com>
2023-06-04 06:23:02 +1000

    feat: fix cellForRowAt Upcoming TableView delegate method, TitleTableViewCell finalised layout

--- NetflixClone/Controllers/UpcomingViewController.swift
+++ NetflixClone/Controllers/UpcomingViewController.swift
@@ -9,32 +9,32 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
     private var titles: [TMDBData] = .init()
-
+    
     private let upcomingTable: UITableView = {
         let table = UITableView()
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        
         return table
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         title = "Upcoming"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
-
+        
         view.addSubview(upcomingTable)
         upcomingTable.delegate = self
         upcomingTable.dataSource = self
-
+        
         fetchUpcoming()
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-
+        
         upcomingTable.frame = view.bounds
     }
 }
@@ -62,10 +62,21 @@ extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return titles.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = titles[indexPath.row].original_title ?? titles[indexPath.row].original_name ?? "Unknown"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(
+            titleName: title.original_title ?? title.original_name ?? "Unknown title",
+            posterURL: title.poster_path ?? ""))
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
 }
--- NetflixClone/Views/TitleTableViewCell.swift
+++ NetflixClone/Views/TitleTableViewCell.swift
@@ -14,6 +14,7 @@ final class TitleTableViewCell: UITableViewCell {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
         return imageView
     }()
 
@@ -25,7 +26,10 @@ final class TitleTableViewCell: UITableViewCell {
 
     private let playTitleButton: UIButton = {
         let button = UIButton()
+        let buttonImage = UIImage(systemName: "play.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(buttonImage, for: .normal)
+        button.tintColor = .label
         return button
     }()
 
@@ -50,9 +54,16 @@ extension TitleTableViewCell {
     private func applyConstraints() {
         NSLayoutConstraint.activate([
             titlePosterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            titlePosterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-            titlePosterImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            titlePosterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            titlePosterImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             titlePosterImageView.widthAnchor.constraint(equalToConstant: 100),
+
+            titleLabel.leadingAnchor.constraint(equalTo: titlePosterImageView.trailingAnchor, constant: 20),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            titleLabel.widthAnchor.constraint(equalToConstant: 200),
+
+            playTitleButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            playTitleButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
         ])
     }
 }
@@ -61,9 +72,8 @@ extension TitleTableViewCell {
 
 extension TitleTableViewCell {
     public func configure(with model: TitleViewModel) {
-        guard let url = URL(string: model.posterURL) else {
-            return
-        }
+        let fullImagePath = "https://image.tmdb.org/t/p/w500\(model.posterURL)"
+        guard let url = URL(string: fullImagePath) else { return }
 
         titlePosterImageView.sd_setImage(with: url, completed: nil)
         titleLabel.text = model.titleName
