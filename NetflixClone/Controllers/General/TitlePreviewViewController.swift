6dc88243ccf877f05bbc79333c9f252497d3cd5e
John Erick Santos <johnsantosdev@gmail.com>
2023-06-04 12:44:20 +1000

    feat: init: TitlePreviewViewController

---
+++ NetflixClone/Controllers/General/TitlePreviewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TitlePreviewViewController.swift
+//  NetflixClone
+//
+//  Created by John Erick Santos on 4/6/2023.
+//
+
+import UIKit
+import WebKit
+
+final class TitlePreviewViewController: UIViewController {
+    private let webView: WKWebView = .init()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
