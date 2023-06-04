e9ce0a2a53d3f88b32bb2d1756a70ac1cc7a3340
John Erick Santos <johnsantosdev@gmail.com>
2023-06-04 12:57:35 +1000

    feat: Dummy data on TitlePreviewViewController. todo: TitlePreviewViewModel

--- NetflixClone/Controllers/General/TitlePreviewViewController.swift
+++ NetflixClone/Controllers/General/TitlePreviewViewController.swift
@@ -11,7 +11,59 @@ import WebKit
 final class TitlePreviewViewController: UIViewController {
     private let webView: WKWebView = .init()
 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.text = "Harry Potter"
+        return label
+    }()
+
+    private let overviewLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
+        label.text = "Overview label"
+        return label
+    }()
+
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemRed
+        button.setTitle("Download", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(webView)
+        view.addSubview(titleLabel)
+        view.addSubview(overviewLabel)
+        view.addSubview(downloadButton)
+
+        applyTitlePreviewConstraints()
+    }
+}
+
+// MARK: - Constraints
+
+extension TitlePreviewViewController {
+    private func applyTitlePreviewConstraints() {
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            webView.heightAnchor.constraint(equalToConstant: 250),
+
+            titleLabel.topAnchor.constraint(equalTo: webView.bottomAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            overviewLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+        ])
     }
 }
